c653783109f471d2e2b6b7c55f16e34ff5704779
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2017-12-20 19:06:16 +0900

    Java Loops II
    Math 클래스 이용하기

---
+++ JavaLoops.java
@@ -0,0 +1,24 @@
+package hackerrank.hackerrank;
+
+import java.util.Scanner;
+
+//Java Loops II
+public class JavaLoops {
+	public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            
+            int result = a;
+            for(int j=0; j<n; j++){
+            	result += Math.pow(2, j)*b;
+            	System.out.print(result+ " ");
+            }
+            System.out.println();
+        }
+        in.close();
+    }
+}
