a074eee3a71e1f96b267ee38e386d135f3f38e14
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2017-12-28 20:18:21 +0900

    Java Substring Comparisons
    Arrays.sort를 이용한 String 배열 정렬

---
+++ JavaSubstringComparisons.java
@@ -0,0 +1,35 @@
+package hackerrank.hackerrank;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+//Java Substring Comparisons
+public class JavaSubstringComparisons {
+	
+	public static String getSmallestAndLargest(String s, int k) {
+        String smallest = "";
+        String largest = "";
+        // Complete the function
+        // 'smallest' must be the lexicographically smallest substring of length 'k'
+        // 'largest' must be the lexicographically largest substring of length 'k'
+        String[] str = new String[s.length()-k+1];
+        int i;
+        for(i=0; i<str.length; i++){
+        	str[i] = s.substring(i, i+k);
+        }
+        Arrays.sort(str);
+        smallest = str[0];
+        largest = str[str.length-1];
+        
+        return smallest + "\n" + largest;
+    }
+	
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+      
+        System.out.println(getSmallestAndLargest(s, k));
+	}
+}
