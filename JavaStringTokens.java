484284ae95f5f6f7db75af028082a9516bdf96ae
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2017-12-29 22:43:27 +0900

    Java String Tokens
    문자열 관련 메소드 사용 방법
    replaceAll을 이용한 문자열 변경
    split를 이용한 문자열 변경

---
+++ JavaStringTokens.java
@@ -0,0 +1,29 @@
+package hackerrank.hackerrank;
+
+import java.util.Scanner;
+
+//Java String Tokens
+public class JavaStringTokens {
+	public static void main(String[] args){
+		 Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        // Write your code here.
+        s = s.replaceAll("'|,|_|\\.|!|\\?|@", " ");
+//        System.out.println("s: "+s);
+        int i, cnt=0;
+        String[] arr = s.split(" ");
+        for(i=0; i<arr.length; i++){
+        	if(!arr[i].equals("")){
+        		cnt++;
+        	}
+        }
+        System.out.println(cnt);
+        for(i=0; i<arr.length; i++){
+        	if(!arr[i].equals("")){
+        		System.out.println(arr[i]);
+        	}
+        }
+        
+        scan.close();
+	}
+}
