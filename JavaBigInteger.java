d9b9d5666ba11c4223fd579ef502639c98d480a9
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2017-12-29 22:55:48 +0900

    Java BigInteger
    BigInteger 클래스를 이용하여 Int 또는 long 보다 훨씬 큰 숫자를 계산하는데 사용하는 클래스이다.

---
+++ JavaBigInteger.java
@@ -0,0 +1,15 @@
+package hackerrank.hackerrank;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+//Java BigInteger
+public class JavaBigInteger {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+        BigInteger a = sc.nextBigInteger();
+        BigInteger b = sc.nextBigInteger();
+        System.out.println(a.add(b));
+        System.out.println(a.multiply(b));
+	}
+}
