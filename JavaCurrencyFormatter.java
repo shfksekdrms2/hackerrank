9ffad66cc72f066773f9f978373d7489c0282653
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2018-01-02 09:38:01 +0900

    Java Currency Formatter
    locale 객체를 이용하여  나라별로 넘버 포멧을 변경한다.

---
+++ JavaCurrencyFormatter.java
@@ -0,0 +1,31 @@
+package hackerrank.hackerrank;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+//Java Currency Formatter
+public class JavaCurrencyFormatter {
+	public static void main(String[] args){
+
+		Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+
+        Locale locale = new Locale("en", "US");
+		NumberFormat Format = NumberFormat.getCurrencyInstance(locale);
+		System.out.println("US: " + Format.format(payment));
+        
+		locale = new Locale("en", "IN");
+		Format = NumberFormat.getCurrencyInstance(locale);
+		System.out.println("India: " + Format.format(payment));
+		
+		locale = new Locale("zh", "CN");
+		Format = NumberFormat.getCurrencyInstance(locale);
+		System.out.println("China: " + Format.format(payment));
+		
+		locale = new Locale("fr", "FR");
+		Format = NumberFormat.getCurrencyInstance(locale);
+		System.out.println("France: " + Format.format(payment));
+	}
+}
