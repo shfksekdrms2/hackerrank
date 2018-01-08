dbbde46a90496ebf3d487f462bdbdbdfc263908f
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2018-01-08 19:56:24 +0900

    Java Map
    map 사용하기

---
+++ JavaMap.java
@@ -0,0 +1,29 @@
+package hackerrank.hackerrank;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+//Java Map
+public class JavaMap {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		in.nextLine();
+		Map<String, Integer> map = new HashMap();
+		for (int i = 0; i < n; i++) {
+			String name = in.nextLine();
+			int phone = in.nextInt();
+			map.put(name, phone);
+			in.nextLine();
+		}
+		while (in.hasNext()) {
+			String s = in.nextLine();
+			if(map.get(s) != null){
+				System.out.println(s + "=" + map.get(s));
+			}else{
+				System.out.println("Not found");
+			}
+		}
+	}
+}
