4b7441617da443950825a2a9783bc8b372ac04de
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2018-01-08 20:44:47 +0900

    Java Stack
    stack 을 이용한 문자열 비교 문제

---
+++ JavaStack.java
@@ -0,0 +1,52 @@
+package hackerrank.hackerrank;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+//Java Stack
+public class JavaStack {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		boolean result = true;
+		Stack<Character> stack = new Stack<>();
+		while (sc.hasNext()) {
+			String input = sc.next();
+			// Complete the code
+			result = true;
+			stack.clear();
+			for (int i = 0; i < input.length(); i++) {
+				char ch = input.charAt(i);
+				if (ch == '{' || ch == '[' || ch == '(') {
+					stack.add(ch);
+				} else {
+					if (!stack.isEmpty()) {
+						if (ch == '}') {
+							if (stack.pop() != '{') {
+								result = false;
+								break;
+							}
+						} else if (ch == ')') {
+							if (stack.pop() != '(') {
+								result = false;
+								break;
+							}
+						} else if (ch == ']') {
+							if (stack.pop() != '[') {
+								result = false;
+								break;
+							}
+						}
+					} else {
+						result = false;
+						break;
+					}
+				}
+			}
+			if (!stack.isEmpty()) {
+				result = false;
+			}
+			System.out.println(result);
+		}
+	}
+}
