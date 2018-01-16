7e657204ceb73880df72f3797a19170427694ee4
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2018-01-16 14:57:16 +0900

    Java Arraylist 활용 문제
    Arraylist를 활용하여 동적 Array 할당

---
+++ JavaArraylist.java
@@ -0,0 +1,34 @@
+package hackerrank.hackerrank;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+//Java Arraylist
+public class JavaArraylist {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		ArrayList<Integer>[] arr = new ArrayList[n];
+		
+		
+		for(int i=0; i<n; i++){
+			int input = sc.nextInt();
+			arr[i] = new ArrayList<>();
+			while(input-- > 0){
+				int value = sc.nextInt();
+				arr[i].add(value);
+			}
+		}
+		
+		int q = sc.nextInt();
+		while(q-- > 0){
+			int row = sc.nextInt()-1;
+			int col = sc.nextInt()-1;
+			try{
+				System.out.println(arr[row].get(col));
+			}catch(Exception e){
+				System.out.println("ERROR!");
+			}
+		}
+	}
+}
