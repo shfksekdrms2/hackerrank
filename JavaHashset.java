6edd522294aba168575c1e069a14b89c776a5189
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2018-01-19 23:30:47 +0900

    Java Hashset
    hashset을 이용하면 중복된 값이 저장되지 않음을 활용

---
+++ JavaHashset.java
@@ -0,0 +1,29 @@
+package hackerrank.hackerrank;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Scanner;
+
+//Java Hashset
+public class JavaHashset {
+	public static void main(String[] args){
+		Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+        String [] pair_left = new String[t];
+        String [] pair_right = new String[t];
+        
+        for (int i = 0; i < t; i++) {
+            pair_left[i] = s.next();
+            pair_right[i] = s.next();
+        }
+        
+
+        HashSet<String> hs = new HashSet<>();
+       
+        for (int i = 0; i < t; i++) {
+        	hs.add("("+pair_left[i]+","+pair_right[i]+")");
+        	System.out.println(hs.size());
+        }
+        
+	}
+}
