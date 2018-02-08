8daf5f00c1285905fe5fca4d414d9a6c050c7f82
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2018-02-08 16:05:47 +0900

    [hackerrank] Closest Numbers
    integer[] 을 int[] 로 바꾸기 위해 1.8 에서 지원하는 stream() 메소드 사용

---
+++ ClosestNumbers.java
@@ -0,0 +1,50 @@
+package hackerrank.hackerrank;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+//Closest Numbers
+public class ClosestNumbers {
+	static int[] closestNumbers(int[] arr) {
+		// Complete this function
+		ArrayList<Integer> list = new ArrayList<>();
+
+		Arrays.sort(arr);
+		int minValue = Integer.MAX_VALUE;
+
+		for (int i = 1; i < arr.length; i++) {
+			if (arr[i] - arr[i - 1] < minValue) {
+				minValue = arr[i] - arr[i - 1];
+			}
+		}
+
+		for (int i = 1; i < arr.length; i++) {
+			if (arr[i] - arr[i - 1] == minValue) {
+				list.add(arr[i - 1]);
+				list.add(arr[i]);
+			}
+		}
+		
+		int[] result = list.stream().mapToInt(Integer::intValue).toArray();
+		return result;
+	}
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int[] arr = new int[n];
+		for (int arr_i = 0; arr_i < n; arr_i++) {
+			arr[arr_i] = in.nextInt();
+		}
+		int[] result = closestNumbers(arr);
+		for (int i = 0; i < result.length; i++) {
+			System.out.print(result[i] + (i != result.length - 1 ? " " : ""));
+		}
+		System.out.println("");
+
+		in.close();
+	}
+}
