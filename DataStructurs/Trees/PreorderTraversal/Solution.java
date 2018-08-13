2bcbd5bb0ab893abdefb3b82eae28b5565377650
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2018-08-13 22:45:11 +0900

    [hackerrank] Preorder Traversal - 전위 순회

---
+++ DataStructurs/Trees/PreorderTraversal/Solution.java
@@ -0,0 +1,58 @@
+package hackerrank.hackerrank.DataStructurs.Trees.PreorderTraversal;
+
+import java.util.*;
+import java.io.*;
+
+/*
+ * https://www.hackerrank.com/challenges/tree-preorder-traversal/problem
+ * Preorder Traversal
+ */
+public class Solution {
+	public static Node insert(Node root, int data) {
+		if (root == null) {
+			return new Node(data);
+		} else {
+			Node cur;
+			if (data <= root.data) {
+				cur = insert(root.left, data);
+				root.left = cur;
+			} else {
+				cur = insert(root.right, data);
+				root.right = cur;
+			}
+			return root;
+		}
+	}
+
+	public static void preOrder(Node root) {
+		if (root != null) {
+			System.out.print(root.data + " ");
+			preOrder(root.left);
+			preOrder(root.right);
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int t = scan.nextInt();
+		Node root = null;
+		while (t-- > 0) {
+			int data = scan.nextInt();
+			root = insert(root, data);
+		}
+		scan.close();
+		preOrder(root);
+	}
+}
+
+class Node {
+	Node left;
+	Node right;
+	int data;
+
+	Node(int data) {
+		this.data = data;
+		left = null;
+		right = null;
+	}
+}
