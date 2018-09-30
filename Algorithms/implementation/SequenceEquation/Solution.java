07aac31a1fdcaa23beac7bc409e4bb70b7a3aca4
shfksekdrms2 <wjdtjdwlsqkqn@gmail.com>
2018-09-30 21:33:44 +0900

    [hackerrank] 규칙을 조금 더 찾아야 함.

---
+++ Algorithms/implementation/SequenceEquation/Solution.java
@@ -0,0 +1,72 @@
+package hackerrank.hackerrank.Algorithms.implementation.SequenceEquation;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+/*
+ * https://www.hackerrank.com/challenges/permutation-equation/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign
+ * Sequence Equation
+ */
+public class Solution {
+	static int[] permutationEquation(int[] p) {
+		int[] result = new int[p.length];
+		// int[] tmp = new int[p.length];
+		// for (int i = 0; i < tmp.length; i++) {
+		// tmp[p[i] - 1] = i + 1;
+		// }
+
+		for (int i = 0; i < result.length; i++) {
+			for (int j = 0; j < result.length; j++) {
+				if (p[p[j] - 1] == p[i]) {
+					result[i] = p[p[j] - 1];
+					break;
+				}
+			}
+
+		}
+
+		return result;
+	}
+
+	private static final Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) throws IOException {
+		// BufferedWriter bufferedWriter = new BufferedWriter(new
+		// FileWriter(System.getenv("OUTPUT_PATH")));
+
+		int n = scanner.nextInt();
+		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+		int[] p = new int[n];
+
+		String[] pItems = scanner.nextLine().split(" ");
+		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+		for (int i = 0; i < n; i++) {
+			int pItem = Integer.parseInt(pItems[i]);
+			p[i] = pItem;
+		}
+
+		int[] result = permutationEquation(p);
+
+		for (int i = 0; i < result.length; i++) {
+			// bufferedWriter.write(String.valueOf(result[i]));
+			System.out.println(String.valueOf(result[i]));
+
+			// if (i != result.length - 1) {
+			// bufferedWriter.write("\n");
+			// }
+		}
+
+		// bufferedWriter.newLine();
+
+		// bufferedWriter.close();
+
+		scanner.close();
+	}
+}
